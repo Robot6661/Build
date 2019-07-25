765a07634b6fc2bf0cb183fe18cc97d1eda043a6
Jyotiraditya <dreadnaught02@outlook.com>
2019-07-26 02:02:04 +0530

    buildbot: build for b2n_sprout
    Signed-off-by: Jyotiraditya <dreadnaught02@outlook.com>

--- clone.sh
+++ clone.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
-git clone  https://github.com/WaveDevices/device_oneplus_oneplus3.git -b p device/oneplus/oneplus3
+git clone https://github.com/Dreadnaught02/device_nokia_B2N_sprout.git -b p device/nokia/B2N_sprout
+git clone https://bitbucket.org/StrangeNoob/vendor_nokia_b2n_sprout.git -b p vendor/nokia/B2N_sprout
+git clone https://github.com/derflacco/android_kernel_drg -b drg_pie kernel/nokia/sdm660
--- config.sh
+++ config.sh
@@ -6,13 +6,13 @@ GITHUB_EMAIL=dreadnaught02@outlook.com
 KBUILD_BUILD_USER="eitri"
 KBUILD_BUILD_HOST="nidavellir"
 
-export oem=oneplus
-export device=oneplus3
+export oem=nokia
+export device=B2N_sprout
 
-ROM="WaveOS"
-manifest_url="https://github.com/Wave-project/manifest"
-export rom_vendor_name="wave" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="p"
+ROM="PixelExperience"
+manifest_url="https://github.com/PixelExperience/manifest"
+export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="pie"
 
 release_repo="Dreadnaught02/wave_builds"
 
