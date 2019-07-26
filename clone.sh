fe7d2b4cac6d1e262425abed049137279d419a6b
Angga R Saputra <kiritoclasher@gmail.com>
2019-07-27 04:25:30 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
-git clone https://github.com/Dreadnaught02/device_nokia_B2N_sprout.git -b p device/nokia/B2N_sprout
-git clone https://bitbucket.org/StrangeNoob/vendor_nokia_b2n_sprout.git -b p vendor/nokia/B2N_sprout
-git clone https://github.com/derflacco/android_kernel_drg -b drg_pie kernel/nokia/sdm660
+git clone https://github.com/Gitskill99/device_xiaomi_kenzo -b pie device/xiaomi/kenzo
+git clone https://github.com/Gitskill99/proprietary_vendor_xiaomi -b pie vendor/xiaomi
+git clone https://github.com/Gitskill99/Escrima_kernel_xiaomi_msm8956 kernel/xiaomi/msm8956
