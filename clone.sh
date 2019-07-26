052760720d23365420544c768dab9518075bb1c8
ascepter <ascepter@students.solano.edu>
2019-07-27 04:58:23 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
-git clone https://github.com/Gitskill99/device_xiaomi_kenzo -b pie device/xiaomi/kenzo
-git clone https://github.com/Gitskill99/proprietary_vendor_xiaomi -b pie vendor/xiaomi
-git clone https://github.com/Gitskill99/Escrima_kernel_xiaomi_msm8956 kernel/xiaomi/msm8956
+git clone https://github.com/amanrajOO7/device_xiaomi_kenzo -b crDroid device/xiaomi/kenzo
+git clone https://github.com/amanrajOO7/proprietary_vendor_xiaomi.git -b pie vendor/xiaomi
+git clone https://github.com/amanrajOO7/Escrima_kernel_xiaomi_msm8956 kernel/xiaomi/msm8956
+git clone https://github.com/amanrajOO7/device_xiaomi_msm8956-common.git -b crDroid device/xiaomi/msm8956-common
